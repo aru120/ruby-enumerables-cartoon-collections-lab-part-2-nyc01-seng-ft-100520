1ba7e90fe5b48f1aff81099491fe266b7ac9270e
Kevin Huang <khuang159@gmail.com>
2020-09-17 16:54:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,7 +32,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.any? do |i| valid_calls.include?(i)
-  return i
+  puts i
     
   end
 end
